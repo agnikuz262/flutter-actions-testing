4443f09851f603a806b3ca1836b3658b341d8c03
Agnieszka Kuźniecow <agnieszka.kuzniecow@appunite.com>
2021-09-24 08:08:33 +0200

    Change text

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Add',
         child: const Icon(Icons.add),
       ),
     );
