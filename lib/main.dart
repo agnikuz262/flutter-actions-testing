37b123b74f2c1616ebf2d9e398edb18b86e85ad4
Agnieszka Kuźniecow <agnieszka.kuzniecow@appunite.com>
2021-09-24 08:28:54 +0200

    Update label

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the button this many times',
             ),
             Text(
               '$_counter',
